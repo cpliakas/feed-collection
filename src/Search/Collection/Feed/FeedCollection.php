a50ecde764df3ae1981e0081f26d2e9a1d00d856
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2013-01-28 17:26:01 -0500

    Added basic feed parser functionality.

---
+++ src/Search/Collection/Feed/FeedCollection.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Feed Collection
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+
+namespace Search\Collection\Feed;
+
+use Search\Collection\SearchCollectionAbstract;
+
+/**
+ * A search collection for RSS / Atom feeds.
+ */
+class FeedCollection extends SearchCollectionAbstract
+{
+    /**
+     * The feed being parsed.
+     *
+     * @var \SimplePie
+     */
+    protected $_feed;
+
+    /**
+     * Implements Search::Collection::SearchCollectionAbstract::init().
+     *
+     * Instantiates a SimplePie object, sets the feed URL if the "url" option
+     * was passed via the constructor.
+     */
+    public function init()
+    {
+        $this->_feed = new SimplePie();
+        if ($url = $this->getOption('url')) {
+            $this->_feed->set_feed_url($url);
+        }
+    }
+
+    /**
+     * Implements Search::Collection::SearchCollectionAbstract::getQueue().
+     */
+    public function getQueue($limit = self::NO_LIMIT)
+    {
+        $this->_feed->init();
+        $items = (array) $this->_feed->get_items();
+        // @todo Better error handling for $items = 0;
+        return new SearchCollectionQueue($items);
+    }
+
+    /**
+     * Returns the SimplePie object.
+     *
+     * @return \SimplePie
+     */
+    public function getFeed()
+    {
+        return $this->_feed;
+    }
+
+    /**
+     * Helper function to set a feed URL.
+     *
+     * @param string|array $url
+     *   A URL or array of URLs.
+     *
+     * @return FeedCollection
+     */
+    public function setFeedUrl($url)
+    {
+        $this->_feed->set_feed_url($url);
+        return $this;
+    }
+}
